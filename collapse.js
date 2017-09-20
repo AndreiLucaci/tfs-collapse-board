df768db9f602acbb345b58616c272d8dd8839448
Andrei Lucaci <contact@andreilucaci.ro>
2017-09-20 17:31:05 +0300

    Added the correct code

--- collapse.js
+++ collapse.js
@@ -1,8 +1,6 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status == 'complete') {
-		var elems = document.querySelectorAll("tr.taskboard-row.taskboard-content-row[id^='taskboard-row-'] div.minimize");
-		for (var i in elems) {
-			elems[i].click();
-		}
-	}
-})
+(function init(){
+    var elems = document.querySelectorAll("tr.taskboard-row.taskboard-content-row[id^='taskboard-row-'] div.minimize");
+    for (var i in elems) {
+        elems[i].click();
+    }
+})();
